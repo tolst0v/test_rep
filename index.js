58c793c2838eff8c7c101f169c8e02a1f2f93c9e
tolst0v <danektolstov@gmail.com>
2022-08-06 09:28:27 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+class Calculator {
+    constructor(a, b) {
+        this.a = a
+        this.b = b
+    }
+
+    sum() {
+        return a + b
+    }
+
+    min() {
+        return a - b
+    }
+}
